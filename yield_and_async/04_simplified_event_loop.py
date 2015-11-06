60a8f633f033b6b29c2703569ef3d1ac2871f35a
Cuiyong <cuiyong02@taikangamc.com.cn>
2015-11-06 13:32:47 +0800

    add some explaination and simplified version

---
+++ yield_and_async/04_simplified_event_loop.py
@@ -0,0 +1,108 @@
+# encoding: utf-8
+
+"""
+    Implement a service which could handle time-based tasks,
+    normal tasks, and read instruction from stdin
+
+    Python 3 only
+"""
+
+from bisect import insort
+from collections import deque
+from functools import partial
+from time import time
+import types
+
+
+def fib(n):
+    if n < 2:
+        yield n
+    else:
+        a = yield fib(n - 1)
+        b = yield fib(n - 2)
+        yield a + b
+
+
+class sleep_for_seconds(object):
+    def __init__(self, wait_time):
+        self._wait_time = wait_time
+
+
+class EventLoop(object):
+    def __init__(self, *tasks):
+        self._running = False
+        self._tasks = deque(tasks)          # Queue of functions scheduled to run
+        self._timers = []
+
+    def schedule(self, coroutine, value=None, stack=(), when=None):
+        """
+            Schedule a coroutine task to run, with value to be sent to it
+            and the stack containing the coroutines that are waiting for the value
+            yielded by this coroutine
+        """
+        'task is wrapped into a partial function, but will not call it now'
+        task = partial(self.resume_task, coroutine, value, stack)
+        'just put it in timers or normal tasks queue'
+        if when:
+            insort(self._timers, (when, task))
+        else:
+            self._tasks.append(task)
+
+    def resume_task(self, coroutine, value=None, stack=()):
+        """
+            Actual tasks handling function
+            It will send value to coroutine and get result from coroutine
+            then schedule new task based on result type
+        """
+        result = coroutine.send(value)
+        if isinstance(result, types.GeneratorType):
+            'result is a generator, which is actually a new coroutine to be scheduled'
+            self.schedule(result, None, (coroutine, stack))
+        elif isinstance(result, sleep_for_seconds):
+            'result is a time-based event, reschedule the result'
+            self.schedule(coroutine, None, stack, time() + result._wait_time)
+        elif stack:
+            'if not result to reschedule, test if there are still coroutines in stack'
+            self.schedule(stack[0], result, stack[1])
+
+    def stop(self):
+        self._running = False
+
+    def do_on_next_tick(self, func, *args, **kwargs):
+        'put the task at the first position of the task queue'
+        self._tasks.appendleft(partial(func, *args, **kwargs))
+
+    def run_forever(self):
+        self._running = True
+        while self._running:
+            'check task queue, one task at one tick from left to right'
+            if self._tasks:
+                task = self._tasks.popleft()
+                task()
+            'time schedule tasks, which are sorted in self._timers'
+            while self._timers and self._timers[0][0] < time():
+                task = self._timers[0][1]
+                'so the next iteration, the next task will be check'
+                del self._timers[0]
+                task()
+        self._running = False
+
+
+def print_every(message, interval):
+    """
+        Task coroutine which will generate sleep_for_seconds obj repeatly
+    """
+    while True:
+        print("{} - {}".format(int(time()), message))
+        yield sleep_for_seconds(interval)
+
+
+def main():
+    loop = EventLoop()
+    hello_task = print_every('Hello', 1)
+    loop.schedule(hello_task)
+    loop.run_forever()
+
+
+if __name__ == '__main__':
+    main()
--- yield_and_async/README.md
+++ yield_and_async/README.md
@@ -1,6 +1,59 @@
 Examples of Python asynchronous manipulations with yield and coroutine
 
 References:
+-------------
 http://sahandsaba.com/combinatorial-generation-using-coroutines-in-python.html
 https://github.com/sahands/coroutine-generation
 http://xidui.github.io/2015/10/29/%E6%B7%B1%E5%85%A5%E7%90%86%E8%A7%A3python3-4-Asyncio%E5%BA%93%E4%B8%8ENode-js%E7%9A%84%E5%BC%82%E6%AD%A5IO%E6%9C%BA%E5%88%B6/
+
+One simple yield example:
+--------------------------
+>>> def fib():
+...     a, b = 0, 1
+...     while True:
+...             y = yield b
+...             print y
+...             a, b = b, a+b
+...
+>>> f = fib()
+>>> next(f)
+1
+>>> print next(f)
+None
+1
+>>> print next(f)
+None
+2
+>>> next(f)
+None
+3
+>>> f.send('aa')
+aa      <--- y will be the value sent to the generator --->
+5
+>>> print f.send('bb')
+bb
+8
+
+Workflow tracking in 03 event loop
+------------------------------------
+Take hello_task as example:
+1. hello_task = print_eveny('Hello', 1), so hello_task is a generator which yield sleep_for_seconds object
+2. loop.schedule(hello_task), make a partial of resume_task func, and append to loop._tasks
+3. loop.run_forever, take task from loop._tasks, and run task(), which will call resume_task(hello_task)
+4. resume_task(hello_task), send None to hello_task generator and get a sleep_for_seconds obj, so schedule hello_task again
+5. schedule with when=sleep_for_seconds._wait_time, so this time hello_task coroutine is wrapped by resume_task and  insorted into loop._timers
+6. loop.run_forever, take task from loop._timers, and run again, which will call resume_task(hello_task) again, and schedule hello_task again
+7. .....
+
+from the workflow, we can find the key points:
+- coroutines are all implemented as generators
+- during schedule, couroutine are wrapped into resume_task partial
+- during running, coroutine generator is triggered by resume_task, and the latter will schedule coroutine again via the result
+- so, the workflow runs like:
+    loop schedule coroutine, which is wrapped into a partial of resume_task func
+        loop take task, and run the resume_task with the coroutine as the 1st parameter
+            -> resume_task trigger coroutine by sending value to coroutine
+               -> coroutine run an iteration and return result to resume_task
+                    -> resume_task checks the result and schedule the coroutine again
+
+
